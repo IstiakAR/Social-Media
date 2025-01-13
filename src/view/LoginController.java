dec191d0f5382aa49495441bdc44e98a732a56fe
IstiakAR <istiakrhyme840@gmail.com>
2025-01-14 00:16:44 +0600

    Updated LoginScreen

--- src/view/LoginController.java
+++ src/view/LoginController.java
@@ -3,16 +3,15 @@ package view;
 
 import main.*;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 
 public class LoginController {
-
-  public void gotoHomepage(ActionEvent event) {
-    try {
-      MainController.gotoHomepage(); // Navigate to the home page
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+  @FXML
+  private TextField loginUsername;
+  @FXML
+  private PasswordField loginPassword;
 
   public void handleForgotPassword(ActionEvent event) {
     System.out.println("Forgot Password clicked!");
@@ -22,5 +21,24 @@ public class LoginController {
   public void handleSignUp(ActionEvent event) {
     System.out.println("Sign Up clicked!");
     // Logic for sign-up goes here
+  }
+
+  public void handleLogin(ActionEvent event) {
+    String username = loginUsername.getText();
+    String password = loginPassword.getText();
+    System.out.println("Login clicked!");
+    if ((Main.users.containsKey(username)){
+      if(Main.users.get(username).password.equals(password)) {
+	System.out.println("Login Matched!");
+    	try {
+          MainController.gotoHomepage();
+    	} catch (Exception e) {
+	  e.printStackTrace();
+      	}
+      }
+      else{
+	System.out.println("Login Failed!");
+      }
+    }
   }
 }
